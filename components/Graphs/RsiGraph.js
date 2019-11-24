9c29f9bacfead5913780d5b6f2b3dade43a099b8
Xelanor <44201710+Xelanor@users.noreply.github.com>
2019-11-24 18:43:18 +0300

    İcons added

--- components/Graphs/RsiGraph.js
+++ components/Graphs/RsiGraph.js
@@ -4,8 +4,67 @@ import { View } from "react-native";
 
 class RsiGraph extends React.PureComponent {
   render() {
-    const data = this.props.data;
+    const data1 = this.props.data;
+    const data2 = [
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80,
+      80
+    ];
+    const data3 = [
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20,
+      20
+    ];
     const contentInset = { top: 20, bottom: 20 };
+
+    const data = [
+      {
+        data: data1,
+        svg: { stroke: "rgb(134, 65, 244)" }
+      },
+      {
+        data: data2,
+        svg: { stroke: "green" }
+      },
+      {
+        data: data3,
+        svg: { stroke: "green" }
+      }
+    ];
 
     return (
       <View style={{ height: 200, flexDirection: "row" }}>
